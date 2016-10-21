2725617d57c542f89209d6cd20486f9307b08a1d
Haxonek <thomash1996@live.com>
2016-10-20 21:21:50 -0500

    CSS fixes/creation

---
+++ js/app.js
@@ -0,0 +1,19 @@
+//var b_s = 0;
+//		var tab = 0;
+//		var ret = 0;
+//		var c = 0;
+//		
+//function convert() {
+//var s = document.getElementById('source').value;
+//						
+//	s = s.replaceAll('"', '\\"');
+//	s = s.replaceAll('\n', '\\n');
+//	s = s.replace('\t', '\\t');
+//	
+//	document.getElementById('result').value = s;
+//}
+//		
+//String.prototype.replaceAll = function(search, replacement) {
+//	var target = this;
+//	return target.replace(new RegExp(search, 'g'), replacement);
+//};
